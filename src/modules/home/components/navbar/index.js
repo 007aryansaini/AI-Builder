f79c348617be9f664e268ee4a5847d9ae4b2b4e9
six-shot <okhuomonajayi54@gmail.com>
2024-04-18 14:03:58 +0000

    fix

--- src/modules/home/components/navbar/index.js
+++ src/modules/home/components/navbar/index.js
@@ -3,23 +3,25 @@ import React from "react";
 export default function Navbar() {
   return (
     <header className="w-full text-white font-clash">
-      
       <div className=" fixed w-full h-[10vh] bg-black/40 z-10 flex items-center">
-
-        <div className="w-full flex justify-between items-center px-[4%]">
-          <h4 className="text-white font-bold text-2xl">LOGO</h4>
-          <nav>
-            <ul className="flex gap-6 items-center text-white ">
-              <li>Home</li>
-              <li>Products</li>
-              <li>Pricing</li>
-              <li>Blog</li>
-              <li>Contact Us</li>
-            </ul>
-          </nav>
-          <div className="flex gap-6  items-center">
-            <button>Login</button>
-            <button className="bg-white text-black px-8 py-2 rounded-[20px]">Sign Up</button>
+        <div className=" max-w-[1440px] mx-auto w-full">
+          <div className="w-full flex justify-between items-center px-[5%]">
+            <h4 className="text-white font-bold text-2xl">LOGO</h4>
+            <nav>
+              <ul className="flex gap-6 items-center text-white ">
+                <li>Home</li>
+                <li>Products</li>
+                <li>Pricing</li>
+                <li>Blog</li>
+                <li>Contact Us</li>
+              </ul>
+            </nav>
+            <div className="flex gap-6  items-center">
+              <button>Login</button>
+              <button className="bg-white text-black px-8 py-2 rounded-[20px]">
+                Sign Up
+              </button>
+            </div>
           </div>
         </div>
       </div>
