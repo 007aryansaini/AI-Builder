9e4a2046b99e351e9be20c92e026bc514fab7c95
six-shot <okhuomonajayi54@gmail.com>
2024-04-18 05:41:34 +0000

    fix

--- src/modules/home/feauture/hero/index.js
+++ src/modules/home/feauture/hero/index.js
@@ -8,24 +8,32 @@ export default function Hero() {
       initial="hidden"
       whileInView="show"
       viewport={{ once: false }}
-      className="w-full h-screen"
+      className="w-full "
     >
-      <motion.div className=" relative  rotate-180 -top-[4%]">
-        <motion.div
-          variants={fadeIn("down", "tween", 0.8, 1)}
-          className=" why w-[100%] sm:h-[200px] h-[200px] "
-        ></motion.div>
-      </motion.div>
+      <div className="w-full h-full relative top-0   ">
+        <motion.div className=" rotate-180 ">
+          <motion.div
+            variants={fadeIn("down", "tween", 0.8, 1)}
+            className=" topp w-[100%] sm:h-[200px] h-[200px] "
+          ></motion.div>
+        </motion.div>
+      </div>
+
       <div className=" flex flex-col justify-center items-center w-full mt-[4%]">
-        <h5 className="text-white btn px-5 py-2 rounded-[50px] font-clash mb-[2%]">Updated: AI Integrated</h5>
+        <h5 className="text-white btn px-5 py-2 rounded-[50px] font-clash mb-[2%]">
+          Updated: AI Integrated
+        </h5>
         <h1 className="text-white font-clash text-[50px] text-center font-bold leading-[50px]">
           Automate Development <br /> AI for memorable Development
         </h1>
         <p className="text-white text-center font-circular text-xl py-[1.5%]">
-          Create unique brand identity,generate and pushlish <br/> consistence while
-          we make devlopment easy as it always <br/>feels good to to things perfectedly
+          Create unique brand identity,generate and pushlish <br /> consistence
+          while we make devlopment easy as it always <br />
+          feels good to to things perfectedly
         </p>
-        <button  className=" bg-[#8d00ce] px-8 rounded-[50px] mt-2 font-clash text-white font-semibold py-2">Try Buildorz</button>
+        <button className=" bg-[#8d00ce] px-8 rounded-[50px] mt-2 font-clash text-white font-semibold py-2">
+          Generate your website
+        </button>
         <div className="relative"></div>
         <div className="gradient-01 absolute w-[30%] inset-0 z-[-10]" />
       </div>
