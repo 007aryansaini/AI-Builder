0ce010cf71c5682fb65187ded3c813399eac1a39
six-shot <okhuomonajayi54@gmail.com>
2024-04-20 02:23:22 +0000

    fix

--- src/modules/home/feauture/hero/index.js
+++ src/modules/home/feauture/hero/index.js
@@ -26,14 +26,14 @@ export default function Hero() {
       <div className="gradient-01 absolute w-[30%] inset-0 z-[-10]" />
 
       <div className="sm:px-0 px-[4%] mt-[6%] flex flex-col justify-center items-center w-full ">
-        <h5 className="text-white btn px-5 py-2 rounded-[50px] font-clash mb-[2%]">
+        <h5 className="text-white btn px-5 py-2 rounded-[50px] font-clash sm:mb-[2%] mb-[4%]">
           Updated: AI Integrated
         </h5>
         <h1 className="text-white font-clash sm:text-[50px] text-[32px] text-center font-bold sm:leading-[50px] leading-[36px]">
           Automate Development <br className="sm:flex hidden" /> AI for
           Development
         </h1>
-        <p className="text-white text-center font-circular sm:text-lg py-[1.5%]">
+        <p className="text-white text-center font-circular sm:text-lg py-4 sm:py-[1.5%]">
           Create unique brand identity,generate and pushlish{" "}
           <br className="sm:flex hidden" /> consistence while we make devlopment
           easy as it always <br className="sm:flex hidden" />
@@ -43,12 +43,12 @@ export default function Hero() {
           onClick={() => {
             setShowModal(true);
           }}
-          className=" bg-[#8d00ce] px-8 rounded-[50px] mt-2 font-clash text-white font-semibold py-2"
+          className=" bg-[#8d00ce] px-8 rounded-[50px] mt-4 sm:mt-2 font-clash text-white font-semibold py-2"
         >
           Generate your website
         </button>
         <GenerateAIModal onClose={handleClose} visible={showModal} />
-        <div className="mt-[5%]">
+        <div className="sm:mt-[5%] mt-[10%]">
           <img className="" src={web3} alt="web3" />
         </div>
       </div>
