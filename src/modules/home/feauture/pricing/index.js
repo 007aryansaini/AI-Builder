c4ec93e5d809f5cd412d1d95a0c66dd8a5150d68
six-shot <okhuomonajayi54@gmail.com>
2024-04-20 05:18:15 +0000

    fix

--- src/modules/home/feauture/pricing/index.js
+++ src/modules/home/feauture/pricing/index.js
@@ -10,7 +10,7 @@ export default function Pricings() {
         <p className="sm:text-xl text-base  font-circular text-center sm:w-[600px]">
           You can get started for free and change your plan as you grow.
         </p>
-        <div className="grid sm:grid-cols-3 gap-8 items-center w-full mt-[5%]">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 items-center w-full mt-[5%]">
           <div>
             <div className="w-full rounded-[14px] bg-[#1A191E] p-5">
               <h5 className="text-xl font-clash font-medium">Free Plan</h5>
