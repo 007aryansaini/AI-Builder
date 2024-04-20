bd4986128f6fd7f02445a552c873c228a237734f
six-shot <okhuomonajayi54@gmail.com>
2024-04-20 03:36:37 +0000

    fix

--- src/modules/generateai/generatedpage/index.js
+++ src/modules/generateai/generatedpage/index.js
@@ -20,7 +20,7 @@ export default function GeneratedPage() {
       ) : (
         <div>
           <div className="text-white max-w-[1440px] mx-auto">
-            <div className="grid grid-cols-4 gap-6 mt-[3%] px-[5%] pt-[12vh]">
+            <div className="grid sm:grid-cols-4 gap-6 mt-[3%] px-[5%] pt-[12vh]">
               <Link to="/generated-template-1">
                 <div className="h-[450px] cursor-pointer">
                   <div className="w-full h-[350px] ">
