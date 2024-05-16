e1b9686827b67461a82dc603238b74739dc3288d
six-shot <okhuomonajayi54@gmail.com>
2024-05-16 10:59:02 +0000

    fix

--- src/modules/demo2/feautures/hero/index.js
+++ src/modules/demo2/feautures/hero/index.js
@@ -1,17 +1,28 @@
-import React from 'react'
-import bgg from "../../assets/hero.png"
-import robot from "../../assets/robot.png"
+import React from "react";
+import bgg from "../../assets/hero.png";
+import robot from "../../assets/robot.png";
+import laptop from "../../assets/laptop.png"
 export default function Hero() {
   return (
     <div className="" style={style.bg}>
       <div className="max-w-[1440px] mx-auto ">
         <div className=" relative px-[5%] ">
           <img src={robot} className="w-[1000px] h-[872px]" alt="robot" />
-          <div className="absolute right-0 top-0 ">
-            <div className=''>
-              <h2 className="text-[48px] text-white font-bold">
+          <div className="absolute right-0 top-[20%] ">
+            <div className="w-[468px]">
+              <h2 className="text-[48px] int font-bold">
                 Introducing the Next-Generation Memecoin Analysis Bot
               </h2>
+              <hr className="w-full h-[10px] bg-white" />
+              <p className="text-lg font-bold text-white mt-[2%]">
+                Combustion is a revolutionary tool that allows you to analyze
+                trending tokens and make informed decisions.
+              </p>
+            </div>
+          </div>
+          <div className="absolute left-0 top-[30%] ">
+            <div className="w-[468px]">
+              <img src={laptop}  alt="laptop" />
             </div>
           </div>
         </div>
