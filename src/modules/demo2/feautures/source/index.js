73fcfdd6433d8a58e52b8b3dad093018df6cb4e7
six-shot <okhuomonajayi54@gmail.com>
2024-05-16 11:28:04 +0000

    fix

--- src/modules/demo2/feautures/source/index.js
+++ src/modules/demo2/feautures/source/index.js
@@ -5,8 +5,8 @@ export default function Source() {
     <div className="w-full bg-[#131C20] py-[4%]">
       <div className="mx-w-[1440px] mx-auto">
         <div className="px-[5%]">
-          <div className="flex items-start gap-3">
-            <img src={refresh} alt="refresh" />
+          <div className="flex items-start gap-4">
+            <img src={refresh} className='w-[40px] mt-[2%]' alt="refresh" />
             <h3 className='text-[40px] text-white font-bold w-[348px] uppercase'>The only Information Source you'll ever need.</h3>
           </div>
         </div>
